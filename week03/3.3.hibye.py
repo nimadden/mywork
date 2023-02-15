ea5e002c09db645a32f071108581f0ff7fe7ec3f
unknown <nimadden@tcd.ie>
2023-02-15 17:38:33 +0000

    week.03more

---
+++ week03/3.3.hibye.py
@@ -0,0 +1,8 @@
+# input string and outputting string
+# author niall madden
+
+#johnsays = str(input("what did john say"))
+#iSaid= str(input("what did I say"))
+#print("John said {}, I said {}".format(johnsays,iSaid))
+message = 'John said \t"hi"\n I said \t"Bye"'
+print(message)
---
+++ week03/3.3.length.py
@@ -0,0 +1,6 @@
+# program that reads a string and outputs how long it is
+# author : Niall Madden
+
+inputString= str(input("enter a word"))
+outputString= len(inputString)
+print(f'the length of {inputString} is {outputString}')
